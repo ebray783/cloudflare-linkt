813df6ceca4a1c179ead5e483e10a432d15d3504
ebray783 <ebray783@gmail.com>
2025-05-20 04:56:29 -0700

    Update wagmi v1 configuration and fix deployment settings

--- app/index-js.js
+++ app/index-js.js
@@ -80,7 +80,6 @@ export default function Home() {
             <button onClick={() => scrollToSection('top')}>Home</button>
             <button onClick={() => scrollToSection('about')}>About</button>
             <button onClick={() => scrollToSection('airdrop-section')}>Claim</button>
-            <button onClick={() => scrollToSection('tokenomics')}>Tokenomics</button>
             <button onClick={() => scrollToSection('contact')}>Contact</button>
           </div>
         </div>
